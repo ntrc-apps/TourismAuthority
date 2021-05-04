ce6bc5c37089277a2b61a25f86eb337c05c93dc8
rnanton <rnanton@ntrcvct.onmicrosoft.com>
2021-05-04 09:56:31 -0400

    The rate app button in the about us page is now functional

--- app/src/main/java/com/example/navigationcomponent/AboutUs.kt
+++ app/src/main/java/com/example/navigationcomponent/AboutUs.kt
@@ -1,12 +1,23 @@
 package com.example.navigationcomponent
 
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AboutUs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
     }
+    fun rateMe (View: View) = try {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + "vc.ntrc.ccyrus.vincywifi")))
+        } catch (e: ActivityNotFoundException) {
+            startActivity( Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=$packageName")))
+        }
 }

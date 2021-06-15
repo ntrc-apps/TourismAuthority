780e9b84512708f80978ebdfb61d66b5c9b83630
rnanton <rnanton@ntrcvct.onmicrosoft.com>
2021-06-15 13:42:08 -0400

    The share location feature passes the longitude and latitude and the link can be opened by the app to open a new activity which shows the location information

---
+++ app/src/main/java/com/example/navigationcomponent/DisplaySharedInfo.kt
@@ -0,0 +1,11 @@
+package com.example.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplaySharedInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_shared_info)
+    }
+}
